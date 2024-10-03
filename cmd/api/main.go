65527e8aabc78b04cff117906c54f4753cf32820
vinh-developer <vinh-developer@192.168.1.6>
2024-10-03 11:59:13 +0700

    set up database, add docker-compose

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/hawful70/example-social/internal/db"
 	"github.com/hawful70/example-social/internal/store"
 	"github.com/joho/godotenv"
 )
@@ -20,9 +21,23 @@ func main() {
 
 	cfg := config{
 		addr: ":" + os.Getenv("ADDR"),
+		db: dbConfig{
+			addr:         os.Getenv("DB_ADDR"),
+			maxOpenConns: 10,
+			maxIdleConns: 10,
+			maxIdleTime:  "15m",
+		},
 	}
 
-	store := store.NewStorage(nil)
+	// Main database
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	log.Println("database connection pool established")
+
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,

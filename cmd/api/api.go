07c00463ae4929ea0745f6c226ca7d9676de3fba
Harry <quangvinh050293@gmail.com>
2024-10-13 15:30:25 +0700

    add middle to authen api

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"log"
 	"net/http"
 	"os"
@@ -103,6 +104,7 @@ func (app *application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+		r.With(app.BasicAuthMiddleware()).Get("/debug/vars", expvar.Handler().ServeHTTP)
 
 		r.Route("/posts", func(r chi.Router) {
 			r.Use(app.AuthTokenMiddleware)
